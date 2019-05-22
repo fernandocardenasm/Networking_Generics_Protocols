9b043fae7ead060b29cf942ed71083c83ef7aee1
Fernando Cardenas <fernando.cardenasm@gmail.com>
2019-05-22 22:44:02 +0200

    first commit

--- Networking_Generics_Protocols/ViewController.swift
+++ Networking_Generics_Protocols/ViewController.swift
@@ -9,12 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        let marvelAPI = MarvelAPI(session: URLSession.shared)
+        marvelAPI.run()
     }
-
-
 }
 
+// This protocol may not be neccessary
+// protocol API {
+//    associatedtype S: Session
+//
+//    var session: S { get }
+//
+//    func run()
+// }
+
+class MarvelAPI<S: Session> {
+    let session: S
+
+    init(session: S) {
+        self.session = session
+    }
+
+    func run() {
+        guard let url = URL(string: "www.apple.com") else {
+            return
+        }
+
+        let task = session.dataTask(with: url) { _, _, _ in
+            print("This was called")
+        }
+        task.resume()
+    }
+
+    func downloadRequest<R: APIRequest>(_ request: R) -> R.Response {
+        return request.response
+    }
+}
+
+protocol APIRequest {
+    associatedtype Response: Decodable
+
+    var response: Response { get }
+}
+
+class Request: APIRequest {
+    let response: [String] = []
+}
+
+protocol Session {
+    associatedtype Task: DataTask
+
+    func dataTask(with url: URL,
+                  completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> Task
+}
+
+extension URLSession: Session {}
+
+protocol DataTask {
+    func resume()
+}
+
+extension URLSessionDataTask: DataTask {}
