fde362766607e63be135231a07a653e74069bc9f
Fernando Cardenas <fernando.cardenasm@gmail.com>
2019-05-22 20:54:45 +0200

    Initial Commit

---
+++ Networking_Generics_Protocols/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Networking_Generics_Protocols
+//
+//  Created by Fernando on 22.05.19.
+//  Copyright © 2019 Fernando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
