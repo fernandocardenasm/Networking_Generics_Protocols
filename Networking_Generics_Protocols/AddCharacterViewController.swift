0ccff72438a000f60c224b4880f2f42512dbb9a5
Fernando Cardenas <fernando.cardenasm@gmail.com>
2019-05-28 22:56:02 +0200

    Isolate classes and protocols in different files

--- Networking_Generics_Protocols/AddCharacterViewController.swift
+++ Networking_Generics_Protocols/AddCharacterViewController.swift
@@ -9,19 +9,18 @@
 import UIKit
 
 class AddCharacterViewController: UIViewController {
-
-    var repositoryService: FirebaseRepositoryService!
+    let repositoryService: FirebaseRepositoryService
 
     init(repositoryService: FirebaseRepositoryService) {
         self.repositoryService = repositoryService
 
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     lazy var addCharacterButtonItem: UIBarButtonItem = {
         let item = UIBarButtonItem(title: "Save",
                                    style: .plain,
@@ -32,7 +31,7 @@ class AddCharacterViewController: UIViewController {
 
     lazy var nameTextField: UITextField = {
         let tf = UITextField()
-        tf.backgroundColor = UIColor(red: 70/255.0, green: 65/255.0, blue: 63/255.0, alpha: 1.0)
+        tf.backgroundColor = UIColor(red: 70 / 255.0, green: 65 / 255.0, blue: 63 / 255.0, alpha: 1.0)
         tf.placeholder = "Write name..."
         tf.textAlignment = .center
         return tf
@@ -41,18 +40,26 @@ class AddCharacterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupUI()
+    }
+
+    private func setupUI() {
+        view.backgroundColor = UIColor(red: 43 / 255.0, green: 43 / 255.0, blue: 45 / 255.0, alpha: 1.0)
+
+        navigationItem.rightBarButtonItem = addCharacterButtonItem
+
+        setupNameTextField()
+    }
+
+    private func setupNameTextField() {
         view.addSubview(nameTextField)
-        
+
         nameTextField.translatesAutoresizingMaskIntoConstraints = false
 
         [nameTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
          nameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
          nameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
          nameTextField.heightAnchor.constraint(equalToConstant: 50)].forEach { $0.isActive = true }
-
-        navigationItem.rightBarButtonItem = addCharacterButtonItem
-
-        view.backgroundColor = UIColor(red: 43/255.0, green: 43/255.0, blue: 45/255.0, alpha: 1.0)
     }
 
     @objc func handleAddCharacter(sender: UIBarButtonItem) {
---
+++ Networking_Generics_Protocols/MarvelAPI.swift
@@ -0,0 +1,179 @@
+//
+//  MarvelAPI.swift
+//  Networking_Generics_Protocols
+//
+//  Created by Fernando Cardenas on 28.05.19.
+//  Copyright © 2019 Fernando. All rights reserved.
+//
+
+import Foundation
+
+// This protocol cannot have the associatedType just the class that implements it
+// Just the method, I did not delete it, so the the comment is more clear.
+// protocol API {
+//    associatedtype S: Session
+//
+//    var session: S { get }
+//
+//    func run()
+// }
+
+class MarvelAPI<S: Session> {
+    let session: S
+
+    init(session: S) {
+        self.session = session
+    }
+
+    func run() {
+        guard let url = URL(string: "www.apple.com") else {
+            return
+        }
+
+        let task = session.dataTask(with: url) { _, _, _ in
+        }
+        task.resume()
+    }
+
+    func downloadResponse<R: APIRequest>(for request: R, completion: @escaping (Result<R.Response, Error>) -> Void) {
+        let endpointURL = makeEndpointURL(for: request)
+
+        guard let url = endpointURL else {
+            completion(.failure(APIError.invalidURL))
+            return
+        }
+        let task = session.dataTask(with: url) { [weak self] data, _, error in
+            if let error = error {
+                print(error)
+                completion(.failure(error))
+            }
+            else if let data = data {
+                guard let convertedData = self?.convertData(data, for: request) else {
+                    completion(.failure(APIError.conversionDataFailed))
+                    return
+                }
+                completion(.success(convertedData))
+            }
+            else {
+                completion(.failure(APIError.unknown))
+            }
+        }
+        task.resume()
+    }
+
+    func convertData<R: APIRequest>(_ data: Data, for request: R) -> R.Response? {
+        guard let marvelResponse = try? JSONDecoder().decode(MarvelResponse<R.Response>.self,
+                                                             from: data),
+            let results = marvelResponse.data?.results else {
+                return nil
+        }
+        return results
+    }
+
+    func makeEndpointURL<T: APIRequest>(for request: T) -> URL? {
+        var components = URLComponents()
+        components.scheme = GlobalConstants.MarvelAPI.scheme
+        components.host = GlobalConstants.MarvelAPI.host
+        components.path = path(for: request)
+        components.port = GlobalConstants.MarvelAPI.port
+        components.queryItems = parameters(for: request)
+        return components.url
+    }
+
+    func path<T: APIRequest>(for request: T) -> String {
+        return GlobalConstants.MarvelAPI.basePath + request.resourceName
+    }
+
+    func parameters<T: APIRequest>(for request: T) -> [URLQueryItem] {
+        return encryptionParameters() + filterParameters(for: request)
+    }
+
+    func encryptionParameters() -> [URLQueryItem] {
+        // Common query items needed for all Marvel requests
+        let timestamp = "\(Date().timeIntervalSince1970)"
+        let hash = "\(timestamp)\(GlobalConstants.MarvelAPI.privateKey)\(GlobalConstants.MarvelAPI.publicKey)".md5()
+        return [
+            URLQueryItem(name: GlobalConstants.MarvelAPI.Parameters.timestamp,
+                         value: timestamp),
+            URLQueryItem(name: GlobalConstants.MarvelAPI.Parameters.hash,
+                         value: hash),
+            URLQueryItem(name: GlobalConstants.MarvelAPI.Parameters.apiKey,
+                         value: GlobalConstants.MarvelAPI.publicKey)
+        ]
+    }
+
+    func filterParameters<T: APIRequest>(for request: T) -> [URLQueryItem] {
+        return request.parameters.map {
+            // converts the value from Any to String
+            URLQueryItem(name: $0.key,
+                         value: String(describing: $0.value))
+        }
+    }
+}
+
+enum APIError: Error {
+    case invalidURL
+    case conversionDataFailed
+    case unknown
+}
+
+protocol APIRequest {
+    associatedtype Response: Decodable
+
+    var resourceName: String { get }
+
+    var parameters: [String: Any] { get }
+}
+
+struct GetComicCharactersRequest: APIRequest {
+    typealias Response = [ComicCharacter]
+
+    var resourceName: String {
+        return GlobalConstants.MarvelAPI.Paths.characters
+    }
+
+    var parameters: [String: Any] {
+        return [GlobalConstants.MarvelAPI.Parameters.limit: limit]
+    }
+
+    let limit: Int
+
+    init(limit: Int = 10) {
+        self.limit = limit
+    }
+}
+
+struct ComicCharacter: Decodable {
+    let id: String?
+    let name: String?
+    let description: String?
+}
+
+struct MarvelResponse<Response: Decodable>: Decodable {
+    public let status: String?
+    public let message: String?
+    public let data: MarvelDataContainer<Response>?
+}
+
+struct MarvelDataContainer<Results: Decodable>: Decodable {
+    public let offset: Int
+    public let limit: Int
+    public let total: Int
+    public let count: Int
+    public let results: Results
+}
+
+protocol Session {
+    associatedtype Task: DataTask
+
+    func dataTask(with url: URL,
+                  completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> Task
+}
+
+extension URLSession: Session {}
+
+protocol DataTask {
+    func resume()
+}
+
+extension URLSessionDataTask: DataTask {}
